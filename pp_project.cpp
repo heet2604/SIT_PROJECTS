4c55a2b8b6b71c331569c8abba340c7627168731
heet2604 <129041286+heet2604@users.noreply.github.com>
2023-10-25 12:44:32 +0530

    Update pp_project.cpp

--- pp_project.cpp
+++ pp_project.cpp
@@ -1,40 +1,37 @@
-#include<iostream>
-#include<iomanip>
-#include<cstdlib>
-#include<Windows.h>
-#include<climits>
-#include<string>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 class User {
 public:
     string username;
-    string password;
+    string password;//
     string userID;
-    string email;
-    string phNO;
-    string location;
-    string name;
+    string email;//
+    long int phNO;//
+    string location;//
+    string name;//
+    string tenanttype;//
+    
+    User() {};
 
-    User() {}
-    
-    User(string name, string email, string phNO, string location, string password)
-        : name(name), email(email), phNO(phNO), location(location), password(password) {
-    }
-    
+    User(string name, string email, long int phNO, string location, string password, string tenanttype, string username )
+        : name(name), email(email), phNO(phNO), location(location), password(password), tenanttype(tenanttype) , username(username){
+    };
+
     void getdata() {
         cout << "Enter User ID: ";
         cin >> userID;
+        cin.ignore();
         cout << "Enter Name: ";
-        //cin.ignore();
         getline(cin, name);
         cout << "Enter email: ";
         cin >> email;
         cout << "Enter Phone number: ";
         cin >> phNO;
-        cout << "Enter your location: ";
         cin.ignore();
+        cout << "Enter your location: ";
         getline(cin, location);
     }
 
@@ -43,24 +40,7 @@ public:
         cout << "Location: " << location << endl;
         cout << "Email: " << email << endl;
         cout << "Phone Number: " << phNO << endl;
-    }
-    
-
-    bool authenticate() {
-        string inputUsername, inputPassword;
-
-        cout << "Enter your username: ";
-        cin >> inputUsername;
-        cout << "Enter your password: ";
-        cin >> inputPassword;
-
-        if (inputUsername == username && inputPassword == password) {
-            cout << "Authentication successful. Welcome, " << username << "!" << endl;
-            return true;
-        } else {
-            cout << "Authentication failed. Please check your credentials." << endl;
-            return false;
-        }
+        cout<<endl<<endl;
     }
 };
 
@@ -68,32 +48,31 @@ class Tenant : public User {
 public:
     int budget;
     int no_of_rooms;
-    string tenanttype;
 
-    Tenant(){};
+    Tenant() {}
 
-    Tenant(string username, string password, string userID, string name, string email, string phNO, string location, int budget, int no_of_rooms, string tenanttype)
-        : User(name, email, phNO, location, password), budget(budget), no_of_rooms(no_of_rooms), tenanttype(tenanttype) {
-        this->username = username;
-        this->userID = userID;
+    Tenant(string username, string password, string userID, string name, string email, long long phNO, string location, int budget, int no_of_rooms, string tenanttype){
+        this->username=username;
+        this->password=password;
+        this->userID=userID;
+        this->name=name;
+        this->email=email;
+        this->phNO=phNO;
+        this->location=location;
+        this->budget=budget;
+        this->no_of_rooms=no_of_rooms;
+        this->tenanttype=tenanttype;
+
     }
 
     void displayTenantProfile() {
-        cout << "Name: " << name << endl;
-        cout << "UserID: " << userID << endl;
-        cout << "Location: " << location << endl;
+        displayProfile();
         cout << "Budget: " << budget << endl;
         cout << "No. of rooms: " << no_of_rooms << endl;
-        cout << "Email: " << email << endl;
-        cout << "Phone Number: " << phNO << endl;
-    }
 
-    void getdata() {
-        User::getdata();
-        cout << "Enter Your Budget: ";
-        cin >> budget;
-        cout << "Enter Your Requirements (1 BHK, 2BHK, 3BHK, etc): ";
-        cin >> no_of_rooms;
+        cout<<"------------------------------------------------------------------------------------------------";
+
+        cout<<endl<<endl;
     }
 };
 
@@ -104,55 +83,28 @@ public:
     string owner_name;
     string owner_id;
 
-    Owner(int no_properties, string tenant_type, string owner_name, string owner_id, string email, string phNO, string location)
-        : no_properties(no_properties), tenant_type(tenant_type), owner_name(owner_name), owner_id(owner_id) {
+    Owner() {};
+
+    Owner(int no_properties, string tenant_type, string owner_name, string owner_id, string email, long long phNO, string location){
+        this->no_properties=no_properties;
+        this->tenant_type=tenant_type;
         this->owner_name=owner_name;
         this->owner_id=owner_id;
-        this->tenant_type=tenant_type;
-        this->no_properties=no_properties;
-        this->email = email;
-        this->phNO = phNO;
-        this->location = location;
-    }
+        this->email=email;
+        this->phNO=phNO;
+        this->location=location;
+    };
 
     void displayOwnerProfile() {
-        //User::displayProfile();
-    
+        name=owner_name;
+        displayProfile();
+        
         cout << "Name: " << owner_name << endl;
         cout << "UserID: " << owner_id << endl;
-        cout << "Location: " << location << endl;
-        cout<<"Number of properties: "<<no_properties<<endl;
-        cout << "Email: " << email << endl;
-        cout << "Phone Number: " << phNO << endl;}
-    
+        cout << "Number of properties: " << no_properties << endl;
 
-    void getProperties() {
-        int no_rooms;
-        int no_bathrooms;
-        string facing;
-        string locality;
-        string property_name;
-        string finishing;
-        string property_type;
-        int price;
-
-        cout << "No. of rooms: ";
-        cin >> no_rooms;
-        cout << "No. of bathrooms: ";
-        cin >> no_bathrooms;
-        cout << "Facing of the property: ";
-        cin >> facing;
-        cout << "Location of the property: ";
-        cin.ignore();
-        getline(cin, locality);
-        cout << "Name of the society: ";
-        getline(cin, property_name);
-        cout << "Furnished or unfurnished? : ";
-        cin >> finishing;
-        cout << "For sale or for rent? : ";
-        cin >> property_type;
-        cout << "Price of the property: ";
-        cin >> price;
+        cout<<"----------------------------------------------------------------------------------------------------------";
+        cout<<endl<<endl;
     }
 };
 
@@ -179,30 +131,37 @@ public:
         cout << "Price of the property: " << price << endl;
         cout << "Facing of the property: " << facing << endl;
         cout << "The property is: " << finishing << endl;
+
+        cout<<"-----------------------------------------------------------------------------------------------------------------";
+        cout<<endl<<endl;
     }
 };
 
-void compareInquiry(Tenant t, Property p) {
-    if (t.no_of_rooms == p.no_rooms && t.location == p.location && t.budget >= p.price) {
-        p.displayProperty();
-    }
+bool compareInquiry(const Tenant& t, const Property& p) {
+    return (t.no_of_rooms == p.no_rooms && t.location == p.location && t.budget == p.price);
 }
 
 int main() {
-        Owner owners[10] = {
-        Owner(5, "Family", "Alice", "2001", "alice@email.com", "1234567890", "Location1"),
-        Owner(3, "Individual", "Bob", "2002", "bob@email.com", "9876543210", "Location2"),
-        Owner(7, "Family", "Charlie", "2003", "charlie@email.com", "5555555555", "Location3"),
-        Owner(2, "Individual", "David", "2004", "david@email.com", "1111111111", "Location4"),
-        Owner(4, "Family", "Eve", "2005", "eve@email.com", "4444444444", "Location5"),
-        Owner(6, "Individual", "Frank", "2006", "frank@email.com", "9999999999", "Location6"),
-        Owner(8, "Family", "Grace", "2007", "grace@email.com", "6666666666", "Location7"),
-        Owner(1, "Individual", "Henry", "2008", "henry@email.com", "7777777777", "Location8"),
-        Owner(5, "Family", "Ivy", "2009", "ivy@email.com", "2222222222", "Location9"),
-        Owner(3, "Individual", "Jack", "2010", "jack@email.com", "3333333333", "Location10")
-    };
+    cout << " ====================================== WELCOME TO TENANT/ OWNER FINDER ========================================\n\n";
 
     
+        Owner o1= Owner(5, "Family", "Alice", "2001", "alice@email.com", 1234567890, "Location1");
+        Owner o2= Owner(3, "Individual", "Bob", "2002", "bob@email.com", 9876543210, "Location2");
+        Owner o3= Owner(7, "Family", "Charlie", "2003", "charlie@email.com", 5555555555, "Location3");
+        Owner o4= Owner(2, "Individual", "David", "2004", "david@email.com", 1111111111, "Location4");
+        Owner o5= Owner(4, "Family", "Eve", "2005", "eve@email.com", 4444444444, "Location5");
+        Owner o6= Owner(6, "Individual", "Frank", "2006", "frank@email.com", 9999999999, "Location6");
+        Owner o7= Owner(8, "Family", "Grace", "2007", "grace@email.com", 6666666666, "Location7");
+        Owner o8= Owner(1, "Individual", "Henry", "2008", "henry@email.com", 7777777777, "Location8");
+        Owner o9= Owner(5, "Family", "Ivy", "2009", "ivy@email.com", 2222222222, "Location9");
+        Owner o10= Owner(3, "Individual", "Jack", "2010", "jack@email.com", 3333333333, "Location10");
+
+        
+        Owner owners[10] = {o1,o2,o3,o4,o5,o6,o7,o8,o9,o10};
+
+    
+
+        
         Property p1=Property(3, 2, "Cozy Cottage", "Suburbia", 150000, "North", owners[0], "House", "Modern");
         Property p2=Property(4, 3, "Luxury Villa", "Seaside", 500000, "East", owners[1], "House", "Luxury");
         Property p3=Property(2, 1, "City Apartment", "Downtown", 100000, "South", owners[2], "Apartment", "Basic");
@@ -217,82 +176,122 @@ int main() {
         Property properties[10]={p1,p2,p3,p4,p5,p6,p7,p8,p9,p10};
     
 
-        Tenant tenant1("user1", "pass1", "ID1", "John Doe", "john@example.com", "1234567890", "Suburbia", 150000, 3, "Family");
-        Tenant tenant2("user2", "pass2", "ID2", "Alice Smith", "alice@example.com", "9876543210", "Downtown", 100000, 2, "Individual");
-        Tenant tenant3("user3", "pass3", "ID3", "Bob Johnson", "bob@example.com", "5555555555", "Beachfront", 250000, 2, "Family");
-        Tenant tenant4("user4", "pass4", "ID4", "Eve Wilson", "eve@example.com", "1111111111", "Lakefront", 220000, 2, "Individual");
-        Tenant tenant5("user5", "pass5", "ID5", "Grace Brown", "grace@example.com", "4444444444", "Seaside", 50000, 3, "Family");
 
-        Tenant tenants[5]={tenant1,tenant2,tenant3,tenant4,tenant5};
+   
+        Tenant t1= Tenant("user1", "pass1", "ID1", "John Doe", "john@example.com", 1234567890, "Suburbia", 150000, 3, "Family");
+        Tenant t2= Tenant("user2", "pass2", "ID2", "Alice Smith", "alice@example.com", 9876543210, "Downtown", 100000, 2, "Individual");
+        Tenant t3= Tenant("user3", "pass3", "ID3", "Bob Johnson", "bob@example.com", 5555555555, "Beachfront", 250000, 2, "Family");
+        Tenant t4= Tenant("user4", "pass4", "ID4", "Eve Wilson", "eve@example.com", 1111111111, "Lakefront", 220000, 2, "Individual");
+        Tenant t5= Tenant("user5", "pass5", "ID5", "Grace Brown", "grace@example.com", 4444444444, "Seaside", 50000, 3, "Family");
 
-        Tenant t;
-        cout<<"\n\nEnter your id : ";
-        cin>>t.userID;
-        cout<<"\nEnter Phone number : ";
-        cin>>t.phNO;
-
-        int j,i;
-
-        for( i=0;i<5;i++){
-            for( j=0;j<5;j++){
-                if(tenants[i].phNO==t.phNO){
-                compareInquiry(tenants[i],properties[j]);
-                }
-                
-            }
-        }
-
-        cout<<endl<<endl;
-
-        properties[j].owner_property.displayOwnerProfile();
-        int choice;
-        cout<<"Press 1 to view image or Press 2 to exit the program : \n\n";
-        cin>>choice;
-        if(choice==1){
-            if(j==1){
-            
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society1.jpeg");}
-
-            if(j==2){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society2.jpeg");
-
-            }
-            if(j==3){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society3.jpeg");
-            }
-            if(j==4){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society4.jpeg");
-            }
-            if(j==5){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society5.jpeg");
-            }
-            if(j==6){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society6.jpeg");
-            }
-
-            if(j==7){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society7.jpeg");
-            }
-
-            if(j==8){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society8.jpeg");
-            }
-
-            if(j==9){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society9.jpeg");
-            }
-            if(j==10){
-                std::system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society1.jpeg");
-            }
-
-                
-            
-        }
-
-        else if(choice==2){
-            cout<<"exiting...";
-            return 0;
-        }
+        Tenant tenants[5]={t1,t2,t3,t4,t5};
         
-        return 0;
+
+
+    int choice = 0;
+
+    while (choice != 10) {
+        std::cout << "\n\nPress 1 to Enter as a tenant" << std::endl;
+        std::cout << "Press 2 to view all properties" << std::endl;
+        std::cout << "Press 3 to display all owner profiles" << std::endl;
+        std::cout << "Press 4 to display all tenant profiles" << std::endl;
+        std::cout <<"Press 5 to display property image using property ID number"<<endl;
+        std::cout << "Press 10 to exit the program" << std::endl;
+        cin>>choice;
+
+
+    if (choice == 1) {
+    // Tenant profile
+
+    Tenant t;
+
+    cout << "Enter user ID: ";
+    cin.ignore();
+    getline(cin, t.userID);
+
+    int flag = 0;
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        if (t.userID == tenants[i].userID) {
+            tenants[i].displayTenantProfile();
+            flag = 1;
+            break; // Exit the loop when the tenant is found
+        }
     }
+
+    if (flag == 0) {
+        cout << "Tenant not found in existing database\n\n";
+    } 
+    
+    else {
+        cout << "Properties most suitable for you:\n\n";
+
+        for (int w = 0; w < 10; w++) {
+            if (compareInquiry(tenants[i], properties[w])) {
+                properties[w].displayProperty();}
+
+
+    }
+    }
+    }
+
+     else if (choice==2){        //displays properties
+            for(int j=0;j<10;j++){
+                properties[j].displayProperty();
+            }
+
+            cout<<endl<<endl;
+        }
+
+        else if(choice==3){         //displays owner profile
+             for(int k=0;k<10;k++){
+                owners[k].displayOwnerProfile();                
+             }
+             cout<<endl<<endl;
+        }
+
+        else if(choice==4){
+             //displays tenant profiles 
+
+             for(int w=0;w<5;w++){
+                tenants[w].displayTenantProfile();
+             }
+        }
+
+    else if (choice == 5) {
+    cout << "Enter property number : " << endl;
+    int prop_num, w;
+    cin >> prop_num;
+    w = prop_num;
+
+    // Property number
+    if (w == 1) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society1.jpeg");
+    } else if (w == 2) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society2.jpeg");
+    } else if (w == 3) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society3.jpeg");
+    } else if (w == 4) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society4.jpeg");
+    } else if (w == 5) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society5.jpeg");
+    } else if (w == 6) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society6.jpeg");
+    } else if (w == 7) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society7.jpeg");
+    } else if (w == 8) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society8.jpeg");
+    } else if (w == 9) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society9.jpeg");
+    } else if (w == 10) {
+        system("start C:\\Users\\HP\\Desktop\\ppprojectpics\\society10.jpeg");
+    }
+}
+
+
+        }
+
+
+    return 0;
+}
